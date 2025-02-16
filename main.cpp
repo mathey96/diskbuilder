a214f877997a5cf87028f199308676467d4f3407
matija <matijazutic96@gmail.com>
2025-02-16 12:33:58 +0100

    we generate table of all possible combinations, without game rule limitations

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <iomanip>
 #define NUM_OF_DISKS 9
 
 struct disk{
@@ -14,12 +16,12 @@ struct general {
 };
 
 general generals[] = {
-	{"mannfred",31},
-	{"isabella",33},
-	{"heinrich",34}
+	{"mannfred", 31},
+	{"isabella", 33},
+	{"heinrich", 34}
 };
 
-disk vc_disks[NUM_OF_DISKS] = {
+std::vector<disk> vc_deck = { // vc one standard set deck
     { "black knights"   ,11   ,'M'  , 2},
     { "banshee"         ,7    ,'S'  , 3},
     { "necromancers"    ,7    ,'S'  , 3},
@@ -31,21 +33,49 @@ disk vc_disks[NUM_OF_DISKS] = {
     { "dire wolves"     ,6    ,'S'  , 3}
 };
 
-int disk_values[NUM_OF_DISKS] = {11,7,7,14,8,9,12,4,6};
-int ways[33] = {0};
 using namespace std;
+
+void findCombinations(vector<disk>& coins, int amount, vector<disk>& currentCombination, vector<vector<disk>>& result, int startIndex) {
+    // If amount becomes 0, we found a valid combination
+    if (amount == 0) {
+        result.push_back(currentCombination);
+        return;
+    }
+
+    // Try all coins starting from the current index
+    for (int i = startIndex; i < coins.size(); i++) {
+        // If the coin is larger than the remaining amount, skip it
+        if (coins[i].command_value > amount) continue;
+
+        // Choose the coin and continue
+        currentCombination.push_back(coins[i]);
+        findCombinations(coins, amount - coins[i].command_value, currentCombination, result, i);  // We can reuse the same coin
+
+        // Backtrack, remove the coin
+        currentCombination.pop_back();
+    }
+}
+
+vector<vector<disk>> coinCombinations(vector<disk>& coins, int amount) {
+    vector<vector<disk>> result;
+    vector<disk> currentCombination;
+
+    // Sort coins to avoid duplicate combinations in the output
+    // sort(coins.begin(), coins.end());
+
+    // Start the recursive search
+    findCombinations(coins, amount, currentCombination, result, 0);
+
+    return result;
+}
 
 int main(int argc, char* argv[]){
 
-	for(int i=0;i < NUM_OF_DISKS; i++)
-		vector<vector<disk>> disk_combinations = 
-	}
-	int i= 0;
-	for(int i =0;i<NUM_OF_DISKS; i++){
-		int j = 0;
-		vc_disks[0] +
-
-
-	}
-
+    vector<vector<disk>> combinations = coinCombinations(vc_deck, 33);
+    for (const auto& combination : combinations) {
+        for (disk coin : combination) {
+            std::cout << "|" << setw(15) << coin.name << "[" << setw(2) << coin.command_value <<"] " <<  left;
+        }
+        cout << "|" << endl;
+    }
 }
